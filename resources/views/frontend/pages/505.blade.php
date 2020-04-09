ff56af2d4a8cc04457741ceae0aec9729196c7cf
Nikolai Gusarov <lime1162@mail.ru>
2020-04-09 20:14:27 +0400

    release v1

---
+++ resources/views/frontend/pages/505.blade.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<meta http-equiv="x-ua-compatible" content="ie=edge">
+	<!-- Font Awesome -->
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
+	<!-- Bootstrap core CSS -->
+	<link href="css/bootstrap.min.css" rel="stylesheet">
+	<!-- Material Design Bootstrap -->
+	<link href="css/mdb.min.css" rel="stylesheet">
+	<!-- Your custom styles (optional) -->
+	<link href="css/style_old.css" rel="stylesheet">
+</head>
+<body>
+	<section id="contend">
+		<div class="container-fluid">
+			<div class="row">
+				<div class="col-12 col-md-4 col-lg-4 col-xl-4">
+					<div class="title-name">На сайте ведутся технические работы</div>
+					<div class="desc-p">
+						<p class="desc">Что-то интересует?</p>
+						<p class="desc">Мы ответим!</p>
+					</div>
+					<div class="test-phone">
+			            <div class="md-form mt-3">
+			                <input type="text" id="materialSubscriptionFormPasswords" class="form-control">
+			                <label for="materialSubscriptionFormPasswords">Ваше имя</label>
+			            </div>
+			            <div class="md-form mt-3">
+			                <input type="text" id="materialSubscriptionFormPhone" class="form-control">
+			                <label for="materialSubscriptionFormPhone">Ваш телефон</label>
+			            </div>
+			            <div class="small-text">
+			            	<p>Продолжая, вы принимаете <a href="#" class="small-txt-a"> условия обработки персональных данных </a></p>
+			            </div>
+			            <div class="btn-send">
+			            	<button class="btn button-send btn-rounded btn-block z-depth-0 my-4 waves-effect" type="submit">Отправить</button>
+			            </div>    
+					</div>
+				</div>
+				<div class="img-size">
+					<div class="col-12 col-md-7 col-lg-7 col-xl-7">
+						<div class="img">
+							<img src="/images/Pick.png" alt="">
+						</div>
+					</div>
+				</div>
+				<div class="col-12 col-md-1 col-lg-1 col-xl-1">
+					
+				</div>
+				
+			</div>
+		</div>
+	</section>
+<!-- JQuery -->
+<script type="text/javascript" src="js/jquery-3.4.1.min.js"></script>
+<!-- Bootstrap tooltips -->
+<script type="text/javascript" src="js/popper.min.js"></script>
+<!-- Bootstrap core JavaScript -->
+<script type="text/javascript" src="js/bootstrap.min.js"></script>
+<!-- MDB core JavaScript -->
+<script type="text/javascript" src="js/mdb.min.js"></script>
+</body>
+</html>
---
+++ resources/views/index.blade.php
@@ -0,0 +1,289 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <title>SIMDEV</title>
+  <!-- Font Awesome -->
+  <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=PT+Sans&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
+  <link rel="stylesheet" href="/css/jquery.mCustomScrollbar.css">
+  <!-- Bootstrap core CSS -->
+  <link href="/css/bootstrap.min.css" rel="stylesheet">
+  <!-- Material Design Bootstrap -->
+  <link href="/css/mdb.min.css" rel="stylesheet">
+  <!-- Your custom styles (optional) -->
+  <link href="/css/style.css" rel="stylesheet">
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+  <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.11.2/jquery-ui.min.js"></script>
+  <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/waypoints/2.0.3/waypoints.min.js"></script>
+  <script type="text/javascript" src="https://cdn.jsdelivr.net/jquery.counterup/1.0/jquery.counterup.min.js"></script>
+<!--   <script type="text/javascript" src="js/jquery-3.4.1.min.js"></script> -->
+</head>
+<body>
+        <header>
+            <nav class="navbar fixed-top top-nav-collapse navbar-expand-lg navbar-dark scrolling-navbar">
+                <div class="container p-3">
+                    <a class="navbar-brand" href="#">
+                        <img src="/images/joxi_screenshot_1586412977699.png" alt="">
+                    </a>
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="navbar-nav ml-auto">
+                            <li class="nav-item pl-5">
+                                <a class="nav-link" href="#Cases">Кейсы <span class="sr-only"></span></a>
+                            </li>
+                            <li class="nav-item pl-5">
+                                <a class="nav-link" href="#examples">Клиенты</a>
+                            </li>
+                            <li class="nav-item pl-5">
+                                <a class="nav-link" href="#in-house">Команда</a>
+                            </li>
+                            <li class="nav-item pl-5">
+                                <a class="nav-link" href="#mail">Контакты</a>
+                            </li>
+                        </ul>
+                        
+                    </div>
+                </div>  
+            </nav>
+            <div id="intro" class="view">
+                <div class="mask rgba-black-strong">
+                    <div class="container h-100">
+                        <div class="row m-t">
+                            
+                                <div class="col-lg-6 col-md-12">
+                                    <p class="display-4 font-weight-bold white-text pt-5 mb-2 my-4 m-p">Создаём удобные сервисы для решения бизнес задач</p>
+                                </div>                              
+                                <hr class="hr-light">
+                                <div class="col-lg-6 col-md-12 m-t-r">
+                                    <p class="my-4 m-p-t">— Исследования, консалтинг и аналитика</p>
+                                    <p class="my-4 m-p-t">— Проектирование, дизайн, разработка</p>
+                                    <p class="my-4 m-p-t">— Опытная in-house команда</p>
+                                </div>
+                        
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </header>
+        <main class="mt-5">
+            <div class="container">
+                <section id="Cases">
+                    <h2 class="mb-5 font-weight-bold title-txt">Кейсы</h2>
+                    <div class="row">
+                        <div class="col-lg-8 col-md-12 mb-4">
+                            <div class="veiw overlay">
+                                <img src="/images/joxi_screenshot_1586346688120.png" alt="" class="cases-col-9">
+                            </div>
+                        </div>
+                        <div class="col-lg-4 col-md-12 mb-4">
+                            <div class="txt">
+                                <p class="cases-col-3">Создание логотипа и брендинг семейной стоматологии</p>
+                            </div>
+                            <div class="pos">
+                                <div class="button-cases">
+                                    <button class="btn btn-cases btn-cases-txt">Branding</button>
+                                    <button class="btn btn-cases btn-cases-txt">Graphic design</button>
+                                <!--    <a href="" class="btn-cases btn-cases-txt mr-2">Branding</a>
+                                    <a href="" class="btn-cases btn-cases-txt">Graphic design</a> -->
+                                </div>
+                            </div>  
+                        </div>
+                        <div class="col-lg-8 col-md-12 mb-4">
+                            <div class="veiw overlay">
+                                <img src="/images/joxi_screenshot_1586348660727.png" alt="" class="cases-col-9">
+                            </div>
+                        </div>
+                        <div class="col-lg-4 col-md-12 mb-4">
+                            <div class="txt">
+                                <p class="cases-col-3">Брендинг и продуктовая разработка сервиса доставки полезной еды</p>
+                            </div>
+                            <div class="pos">
+                                <div class="button-cases">
+                                    <button class="btn btn-cases btn-cases-txt">Frontend</button>
+                                    <button class="btn btn-cases btn-cases-txt">Development</button>
+                                </div>
+                                <div class="button-cases">
+                                    <button class="btn btn-cases btn-cases-txt">Web design</button>
+                                    <button class="btn btn-cases btn-cases-txt">Graphic design</button>
+                                </div>
+                            </div>
+                            
+                        </div>
+                    </div>
+                </section>
+                <section id="statistics">
+                    <h2 class="mb-5 font-weight-bold title-txt">Статистика</h2>
+                    <div class="row">
+                        <div class="col-md-3" >
+                            <span class="counter txt-static">15</span><span class="txt-static"> проектов</span>
+                            <hr style="background-color: #fff;">
+                            <p class="txt-small">15 крупных заверщённых проектов федерального уровня</p>
+                        </div> 
+                        <div class="col-md-3">
+                            <span class="counter txt-static">20</span><span class="txt-static">+ сотрудников</span>
+                            <hr style="background-color: #fff;">
+                            <p class="txt-small">In-house комманда из более чем 30 сотрудников</p>  
+                        </div>
+                        <div class="col-md-3">
+                            <span class="counter txt-static">140</span><span class="txt-static">+ лет</span>
+                            <hr style="background-color: #fff;">
+                            <p class="txt-small">Общий опыт работы сотрудников компании</p>
+                        </div>
+                        <div class="col-md-3">
+                            <span class="counter txt-static">20</span><span class="txt-static"> клиентов</span>
+                            <hr style="background-color: #fff;">
+                            <p class="txt-small">20 клиентов, доверяющих нашим компетенциям</p>
+                        </div>
+                    </div>
+                </section>
+                
+                <section id="examples">
+
+                    <h2 class="mb-5 font-weight-bold title-txt">Клиенты</h2>
+
+                    <div class="row">
+                        <div class="col-lg-3 col-md-12 mb-4">
+                            <div class="veiw overlay zoom">
+                                <img src="/images/joxi_screenshot_1586409954724.png" alt="" class="img-fluid">
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-md-12 mb-4">
+                            <div class="veiw overlay zoom">
+                                <img src="/images/joxi_screenshot_1586409954724.png" alt="" class="img-fluid">
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-md-12 mb-4">
+                            <div class="veiw overlay zoom">
+                                <img src="/images/joxi_screenshot_1586409954724.png" alt="" class="img-fluid">
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-md-12 mb-4">
+                            <div class="veiw overlay zoom">
+                                <img src="/images/joxi_screenshot_1586409954724.png" alt="" class="img-fluid">
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-md-12 mb-4">
+                            <div class="veiw overlay zoom">
+                                <img src="/images/joxi_screenshot_1586409954724.png" alt="" class="img-fluid">
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-md-12 mb-4">
+                            <div class="veiw overlay zoom">
+                                <img src="/images/joxi_screenshot_1586409954724.png" alt="" class="img-fluid">
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-md-12 mb-4">
+                            <div class="veiw overlay zoom">
+                                <img src="/images/joxi_screenshot_1586409954724.png" alt="" class="img-fluid">
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-md-12 mb-4">
+                            <div class="veiw overlay zoom">
+                                <img src="/images/joxi_screenshot_1586409954724.png" alt="" class="img-fluid">
+                            </div>
+                        </div>
+                    </div>
+                
+                </section>
+                <section id="in-house">
+                    <h2 class="mb-5 font-weight-bold title-txt">In-house команда</h2>
+                    <div class="row">
+                        <div class="col-lg-6 col-md-12 mb-4 m-b-s">
+                            <p class="fio-house">Дмитрий Митричев</p>
+                            <hr style="background-color:#fff;width: 300px;margin: unset;margin-bottom: 10px;">
+                            <p class="small-house">Начальник отдела разработки</p>
+                        </div>
+                        <div class="col-lg-6 col-md-12 mb-4">
+                            <div class="veiw overlay zoom">
+                                <img src="/images/joxi_screenshot_1586353720608.png" alt="" class="img-fluid">
+                            </div>
+                        </div>
+                        <div class="col-lg-6 col-md-12 mb-4 m-b-s">
+                            <p class="fio-house">Игорь Харовский</p>
+                            <hr style="background-color:#fff;width: 300px;margin: unset;margin-bottom: 10px;">
+                            <p class="small-house">Product designer</p>
+                        </div>
+                        <div class="col-lg-6 col-md-12 mb-4 ">
+                            <div class="veiw overlay zoom">
+                                <img src="/images/joxi_screenshot_1586353720608.png" alt="" class="img-fluid">
+                            </div>
+                        </div>
+                        <div class="col-lg-6 col-md-12 mb-4 m-b-s">
+                            <p class="fio-house">Николай Гусаров</p>
+                            <hr style="background-color:#fff;width: 300px;margin: unset;margin-bottom: 10px;">
+                            <p class="small-house">Frontend delevoper</p>
+                        </div>
+                        <div class="col-lg-6 col-md-12 mb-4">
+                            <div class="veiw overlay zoom">
+                                <img src="/images/joxi_screenshot_1586353720608.png" alt="" class="img-fluid">
+                            </div>
+                        </div>
+                        <div class="col-lg-6 col-md-12 mb-4 m-b-s">
+                            <p class="fio-house">Айрат Исмагилов</p>
+                            <hr style="background-color:#fff;width: 300px;margin: unset;margin-bottom: 10px;">
+                            <p class="small-house">Backend developer</p>
+                        </div>
+                        <div class="col-lg-6 col-md-12 mb-4">
+                            <div class="veiw overlay zoom">
+                                <img src="/images/joxi_screenshot_1586353720608.png" alt="" class="img-fluid">
+                            </div>
+                        </div>
+                    </div>
+                </section>
+                <section id="mail">
+                    <h2 class="mb-5 font-weight-bold title-txt">Напишите нам</h2>
+                        <div class="test-phone d-flex-p">
+                            <div class="col-lg-6 col-md-12 p-l">
+                                <div class="md-form mt-3">
+                                    <input type="text" id="materialSubscriptionFormPasswords" class="form-control">
+                                    <!-- <span class="some-form__hint">Обязательно для заполнения</span> -->
+                                    <label for="materialSubscriptionFormPasswords" style="font-size: 20px; line-height: 5px;">Ваше имя</label>
+                                </div>
+                            </div>
+                            <div class="col-lg-6 col-md-12 p-l">
+                                <div class="md-form mt-3">
+                                    <input id="userPhone" name="phone" type="tel" data-validate class="form-control">
+                                    <!-- <span class="some-form__hint">Обязательно для заполнения</span> -->
+                                    <label for="materialSubscriptionFormPhone mailp" style="font-size: 20px; line-height: 5px;">Ваш телефон</label>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="mail-small">
+                            <p>Продолжая, вы принимаете условия обработки<a href="#" class="mail-small-a"> персональных данных </a></p>
+                        </div>
+                </section>
+                <section id="footer">
+                    <p class="mail-txt">info@simdev.ru</p>
+                </section>
+            </div>
+        </main>
+
+
+<!-- JQuery -->
+<script type="text/javascript" src="/js/test.js"></script>
+<!-- Bootstrap tooltips -->
+<script type="text/javascript" src="/js/popper.min.js"></script>
+<!-- Bootstrap core JavaScript -->
+<script type="text/javascript" src="/js/bootstrap.min.js"></script>
+<!-- MDB core JavaScript -->
+<script type="text/javascript" src="/js/mdb.min.js"></script>
+<script src="/js/inputmask.min.js"></script>
+<script src="/js/jquery.inputmask.min.js"></script>
+
+<script>
+  $(document).ready(function(){
+  
+  $('input[type="tel"]').inputmask('+7 (999) 999 99 99', {
+    clearMaskOnLostFocus: true,
+  });
+  
+});
+</script>
+</body>
+</html>
